6bfaeb1d773e14af927eb2319e0dafcdcbceec49
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-07-26 00:19:22 -0500

    initial commit

---
+++ process_args.c
@@ -0,0 +1,19 @@
+/*********************
+Name: Rebecca Mckeever
+Course: CS 344
+Assignment 3
+Last edited: 07/26/2021
+**********************/
+
+#include "smallsh.h"
+
+
+//int main(int argc, char *argv[]){
+int process(struct cmd_line *cmd_parts) {
+    int result;
+
+
+
+    //exit(EXIT_SUCCESS);
+    return result;
+}
