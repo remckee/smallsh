2722e42df1df4b8327db0ab2062fbe3346bb7625
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-07-28 18:10:40 -0500

    removed commented code

--- redirection.c
+++ redirection.c
@@ -24,34 +24,10 @@ int redirect(char *file_name, char *msg, int flags, int new_fd) {
 
 
 int redirect_input(char *file_name) {
-    /* int success = -1; */
-    /* int input_fd = open(file_name, O_RDONLY, S_IRW); */
-
-    /* if (input_fd == -1) { */
-    /*     success = input_fd; */
-    /*     fprintf(stderr, "cannot open %s for input\n", file_name); */
-
-    /* } else { */
-    /*     success = dup2(input_fd, STDIN_FILENO); */
-    /* } */
-    /* return success; */
-
     return redirect(file_name, "input", O_RDONLY, STDIN_FILENO);
 }
 
 
-
 int redirect_output(char *file_name) {
-    /* int success = -1; */
-    /* int output_fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRW); */
-
-    /* if (output_fd == -1) { */
-    /*     success = output_fd; */
-    /*     fprintf(stderr, "cannot open %s for output\n", file_name); */
-
-    /* } else { */
-    /*     success = dup2(output_fd, STDOUT_FILENO); */
-    /* } */
-    /* return success; */
     return redirect(file_name, "output", O_WRONLY | O_CREAT | O_TRUNC, STDOUT_FILENO);
 }
